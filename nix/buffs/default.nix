8f1af4f72cc3b10ec39a722186aa71017a6110cc
ooks-io <ooks@protonmail.com>
2025-07-20 14:13:02 +1000

    nix: implement buff definitions

---
+++ nix/buffs/default.nix
@@ -0,0 +1,45 @@
+let
+  buffs = {
+    full = {
+      trueshotAura = true;
+      unholyAura = true;
+      darkIntent = true;
+      moonkinAura = true;
+      leaderOfThePack = true;
+      blessingOfMight = true;
+      legacyOfTheEmperor = true;
+      bloodlust = true;
+      stormlashTotemCount = 5;
+      skullBannerCount = 3;
+    };
+    caster = {
+      serpentsSwiftness = true;
+      arcaneBrilliance = true;
+      moonkinAura = true;
+      leaderOfThePack = true;
+      blessingOfMight = true;
+      blessingOfKings = true;
+      bloodlust = true;
+      manaTideTotemCount = 1;
+      stormlashTotemCount = 4;
+      skullBannerCount = 2;
+    };
+    fullNoLust = buffs.full // {bloodlust = false;};
+    none = {
+      trueshotAura = false;
+      unholyAura = false;
+      darkIntent = false;
+      moonkinAura = false;
+      leaderOfThePack = false;
+      blessingOfMight = false;
+      legacyOfTheEmperor = false;
+      bloodlust = false;
+      stormlashTotemCount = 0;
+      skullBannerCount = 0;
+    };
+    custom = opts: buffs.full // opts;
+  };
+in {
+  flake.buffs = buffs;
+  _module.args.buffs = buffs;
+}
