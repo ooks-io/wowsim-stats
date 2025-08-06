e10e9bb66ba81d88d77099270bd2d369f9bebb51
ooks-io <ooks@protonmail.com>
2025-08-06 19:19:15 +1000

    nix (classes): brewmaster init

--- nix/classes/monk/default.nix
+++ nix/classes/monk/default.nix
@@ -18,5 +18,5 @@
   ];
   # Monk specs
   windwalker = import ./windwalker.nix {inherit lib consumables;};
-  #brewmaster = import ./brewmaster.nix {inherit lib consumables;};
+  brewmaster = import ./brewmaster.nix {inherit lib consumables;};
 }
