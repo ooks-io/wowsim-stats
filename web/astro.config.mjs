a0f3cf4ec941e51618e663094d8315852aff9e19
ooks-io <ooks@protonmail.com>
2025-09-03 21:10:13 +1000

    web: gitignore node_modules
    how did i forget this

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wowsimstats.com",
+  vite: {
+    server: {
+      // Add headers for SQLite database support
+      headers: {
+        'Accept-Ranges': 'bytes'
+      }
+    }
+  }
 });
